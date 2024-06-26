1f0ba633ee268585f7407cd26d67344407828e06
Gennady Poplevin <93812078+genpoplevin@users.noreply.github.com>
2024-06-26 10:21:12 +0500

    Add files via upload

---
+++ case_3/script.js
@@ -0,0 +1,23 @@
+// Инициализируем Swiper
+new Swiper('.image-slider', {
+    // Стрелки
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    // Текущее положение
+    pagination: {
+        el: '.swiper-pagination',
+        // Фракция
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return 'Картинка <span class="' 
+                   + currentClass + '"></span>' 
+                   + ' из ' + '<span class="' 
+                   + totalClass + '"></span>'
+        },
+        
+    },
+    // Бесконечный слайдер
+    loop: true,
+});
